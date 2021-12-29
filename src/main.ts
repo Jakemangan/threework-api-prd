f5e7cfaf2140c22e08c642d308a1b46062a0f649
Jakemangan <jakemangan1@hotmail.co.uk>
2021-12-29 20:37:41 +0000

    Tidyup

--- src/main.ts
+++ src/main.ts
@@ -20,33 +20,31 @@ const knex = require('knex')({
 });
 
 async function bootstrap() {
-  console.log(process.env.SERVICE_KEY);
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    }),
-  );
-  // app.enableCors({
-  //   origin: ['http://localhost:3000'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // app.use(
+  //   cors({
+  //     origin: '*',
+  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //     preflightContinue: false,
+  //     optionsSuccessStatus: 204,
+  //   }),
+  // );
+  // // app.enableCors({
+  // //   origin: ['http://localhost:3000'],
+  // //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // // });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
   // });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
 
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
   });
 
-  console.log('DATABASE_URL: ', process.env.DATABASE_URL);
   app.use(json({ limit: '5mb' }));
   app.use(urlencoded({ extended: true, limit: '5mb' }));
   await app.listen(process.env.PORT || 5050);
