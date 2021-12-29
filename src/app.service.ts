ce9e38fc82b59a23876e16f8c3799659b84a7f65
Jake Mangan <jakemangan1@hotmail.co.uk>
2021-12-29 19:53:41 +0000

    Heroku

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World fuck off!';
+    return 'Hello World fuck offwfe4uh9pfguib4euybhoguhboewhuieogrfbh!';
   }
 }
